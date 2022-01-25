678bb5f363b2710088e66b947cb9583048cab305
alexfrias <alexfrias1226@gmail.com>
2022-01-24 23:29:20 -0500

    pushing what I have so I dont lost it

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,7 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+import "components"
 
 import LocalTime from 'local-time'
 LocalTime.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,12 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 pin "local-time" # @2.1.0
+
+# React
+pin "htm", to: "https://ga.jspm.io/npm:htm@3.1.0/dist/htm.module.js"
+pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@17.0.2/index.js"
+pin "object-assign", to: "https://ga.jspm.io/npm:object-assign@4.1.1/index.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.20.2/index.js"
+
+pin_all_from "app/javascript/components", under: "components"
